be4d740111f646440e1466908e450169bf394908
meljoi <uradgal@gmail.com>
2020-07-12 22:02:05 -0400

    made changes to the random ship placment loop

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const battleship = () => {
-
   //step 1
 
   //objects of players(works)
@@ -25,87 +24,87 @@ const battleship = () => {
   }
 
 
-  //step 2(notice number of placed ships are not always consistent when ran. sometimes it shows 4 other times 3 maybe a two. not sure why. if you see what's wrong, please give me feedback. thanks!
+  //step 2(works)
 
-  //setup of ships
+  //setup of ships using the random coordinate placments for each players board.
 
-    let ships;
+    let ships=0;
     let xPos;
     let yPos;
-      for (i=0; i<player1.shipCount; i++){
+
+    while(ships < player1.shipCount){
       xPos = Math.floor(Math.random() * 4);
       yPos = Math.floor(Math.random() * 4);
         if(parseInt(player1.gameBoard[xPos][yPos])!==1){
         player1.gameBoard[xPos][yPos] = 1;
         ships++;
         }
-      }
 
-      for (i=0; i<player2.shipCount; i++){
+    }
+
+    while(ships < player2.shipCount){
       xPos = Math.floor(Math.random() * 4);
       yPos = Math.floor(Math.random() * 4);
         if(parseInt(player2.gameBoard[xPos][yPos])!==1){
-          player2.gameBoard[xPos][yPos] = 1;
+        player2.gameBoard[xPos][yPos] = 1;
         ships++;
         }
-      }
+    }
 
-// i feel logically that if player1 gameboard is shown then player2 should enter those coordinates to win the game not the other way arround. am i right? if so not sure how to fix.
-  console.log(player1.gameBoard.toString())
+
+
+
+  //console.log(player1.gameBoard.toString())
 
 
   //step 3
-  //play game
+  // play game. Used a remainder to switch between players. conditional checks if players coordinants match random
 
   //function playGame(){
-  let currentPlayer;
-  let otherPlayer;
-  let count=0;
-    while(true){
-    if(+(count%2)===0){
-    currentPlayer = player1;
-    otherPlayer = player2;
-    }else{
-    currentPlayer = player2;
-    otherPlayer = player1;
-    }
+    let currentPlayer;
+    let otherPlayer;
+    let count=0;
 
+      while(true){
+        if(+(count%2)===0){
+        currentPlayer = player1;
+        otherPlayer = player2;
+        }else{
+        currentPlayer = player2;
+        otherPlayer = player1;
+        }
 
-  let qxp = prompt(`${currentPlayer.name} Please choose your x coordinate`);
-  let qyp = prompt(`${currentPlayer.name} Please choose your y coordinate`);
-// it seems to not be changing from 1 to 0. i can hit the same coordinate twice and be a hit.
-    if(currentPlayer.gameBoard[qxp][qyp]===1){
-    alert("hit");
-    otherPlayer.shipCount --;
-     }else{
-     alert("miss. Next Player");
+        let qxp = prompt(`${currentPlayer.name} Please choose your x coordinate`);
+        let qyp = prompt(`${currentPlayer.name} Please choose your y coordinate`);
 
-     }count++;
+        if(currentPlayer.gameBoard[qxp][qyp]===1){
+        alert("hit");
+        otherPlayer.shipCount --;
+        }else{
+         alert("miss. Next Player");
+         }count++;
 
-    if(player1.shipCount===0 || player2.shipCount===0){
-      alert("Game Over");
-      break;
-     }
+        if(player1.shipCount===0 || player2.shipCount===0){
+          alert("Game Over");
+          break;
+         }
+       }
 
-  }
-  // console.log(currentPlayer.shipCount)
+  //declare winner. conditional checks if any players shipcount is 0. is yes the opponant wins.
 
-  //declare winner
+    let winner;
+      if(parseInt(player1.shipCount)===0){
+        winner = currentPlayer;
+      }else if (parseInt(player2.shipCount)===0){
+        alert(`${player1.name} is the winner`);
+        winner = currentPlayer;
+       } else{
+        alert("Error, check your coordinates");
+      }
+       return `The winner is ${winner.name}!`
 
-  let winner;
-    if(parseInt(player1.shipCount)===0){
-      winner = currentPlayer;
-       //alert(`${player2.name} is the winner`);
+  //}//playGame()
 
-    }else if (parseInt(player2.shipCount)===0){
-      alert(`${player1.name} is the winner`);
-      winner = currentPlayer;
-     } else{
-      alert("Error, check your coordinates");
-    }
-    return `The winner is ${winner.name}!`;
- //}
-//playGame()
 }
 
 const gameResult = battleship()
